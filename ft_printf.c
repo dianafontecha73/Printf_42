344f5e12edf25a274930e76b7088dd102591da83
dianafontecha73 <dianafontecha73@gmail.com>
2024-02-22 18:42:34 +0100

    Iniicio de función puntero

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dfontech <dfontech@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 16:59:15 by dfontech          #+#    #+#             */
-/*   Updated: 2024/02/21 18:05:37 by dfontech         ###   ########.fr       */
+/*   Updated: 2024/02/22 18:39:55 by dfontech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,10 +47,10 @@ int	ft_printf(const char *cadena, ...)
 { // donde: FORMATO es una cadena qque contiene un texto que se escribir´en la salid estándar.  ARGUMENTOS ADICIONALES (tres punts se llaman elipses) que indica el número variable de argumentos dependiendo del formato de la cadena//
 	va_list va; // lista de argumentos variables.
 	int	i;
-	int length; //para contar el número de caracteres impresos, función principal de printf
+	int len; //para contar el número de caracteres impresos, función principal de printf
 	
 	i = 0;
-	length = 0;
+	len = 0;
 	va_start(va, cadena); // para inicializar con la lista de argumentos
 	if(!cadena)
 		return (0);
@@ -59,12 +59,12 @@ int	ft_printf(const char *cadena, ...)
 		if(cadena[i] == '%')
 		{
 			i++;
-			length += ft_format(va, &cadena[i]); //& es un operador de referencia, se utiliza para almacenar la dirección de memoria de la variable y la asigna al puntero
+			len += ft_format(va, &cadena[i]); //& es un operador de referencia, se utiliza para almacenar la dirección de memoria de la variable y la asigna al puntero
 		}
 		else
-			length += ft_putchar(cadena[i]); // imprimir el caracter recibido e incrementar el valor apuntaddo por counter para mostrar el número toal de caracteres impresos
+			len += ft_putchar(cadena[i]); // imprimir el caracter recibido e incrementar el valor apuntaddo por counter para mostrar el número toal de caracteres impresos
 		i++;
 	}
 	va_end(va); //para limpiar la lista de caracteres
-	return(length); // retorna número total de caracteres	
+	return(len); // retorna número total de caracteres	
 }
